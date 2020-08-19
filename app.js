68cfd9b359336bbeb36fed28958a70936daee3d4
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-08-19 10:21:26 -0300

    First Module

---
+++ app.js
@@ -0,0 +1,33 @@
+// Aviso qual a classe que vou/quero requerer
+// Coloco uma variável
+const counter = require('./count');
+
+console.log(counter([1,2,3]));
+//console.log(teste(1,2,3));
+
+// ----------------------type of functions -
+// // Normal function statement
+// function sayHi(){
+//   console.log('hu');
+// }
+
+// // Function expression (Anonimous function)
+// const sayBye = function(){
+//   console.log('bye');
+// };
+
+// sayBye();
+// ----------------------type of functions -
+
+
+// let time = 0;
+
+// const timer = setInterval(function () {
+//   time += 2;
+//   console.log(`${time} seconds have passed`);
+
+//   if (time > 5) {
+//     console.log("enrou?");
+//     clearInterval(timer);
+//   }
+// }, 2000);
---
+++ count.js
@@ -0,0 +1,17 @@
+// método de execução do módulo de contagem
+
+
+// const teste = function(inteiro1, inteiro2, inteiro3){
+//     console.log('entrou no teste');
+//     return 'este é o inteiro';
+// };
+
+const counter = function(arr){
+    console.log('entrou no counter');
+    return `There are ${arr.length} elements in this array`;
+};
+// Qual é o método que quero exportar
+
+
+module.exports = counter;
+//module.exports = teste;
