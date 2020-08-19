73141ecbe46bad9ac84a56be05b5d75a8bbdeffb
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-08-19 20:28:19 -0300

    lesson #12

--- app.js
+++ app.js
@@ -1,10 +1,64 @@
-// Aviso qual a classe que vou/quero requerer
-// Coloco uma variável
-const counter = require('./count');
+//#12 - Creating a http server in node to test
+const http = require("http");
 
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end('Hey guys"');
+});
 
-console.log(counter.counter([1,2,3]));
-console.log(counter.teste(1,2,3));
+server.listen(3000, "127.0.0.1");
+console.log("oprt 3000 listening");
+
+// //#9
+// const fs = require("fs");
+
+// const readMe = fs.readFile("readme.md", "utf8", function(err,data){
+//     console.log(readMe);
+// });
+
+//fs.writeFileSync('readme.md','utf8',)
+
+// #8
+// const events = require("events");
+// const util = require("util");
+
+// const Person = function (name) {
+//   this.name = name;
+// };
+
+// util.inherits(Person, events.EventEmitter);
+
+// const James = new Person("James");
+// const Mary = new Person("Mary");
+// const Ryu = new Person("Ryu");
+// const people = [James, Mary, Ryu];
+
+// people.forEach(function (person) {
+//   console.log("log do foreach");
+//   person.on("speak", function (mssg) {
+//     console.log("log de quem tem a msg");
+//     console.log(`${person.name} said ${mssg}`);
+//   });
+// });
+
+// James.emit("speak", "hey dudes!");
+
+// const events = require("events");
+
+// const myEmitter = new events.EventEmitter();
+
+// myEmitter.on("someEvent", function (mssg) {
+//   console.log(mssg);
+// });
+
+// myEmitter.emit('someEvent','The event was emitted');
+
+// // Aviso qual a classe que vou/quero requerer
+// // Coloco uma variável
+// const counter = require('./count');
+
+// console.log(counter.counter([1,2,3]));
+// console.log(counter.teste(1,2,3));
 
 // ----------------------type of functions -
 // // Normal function statement
@@ -19,7 +73,6 @@ console.log(counter.teste(1,2,3));
 
 // sayBye();
 // ----------------------type of functions -
-
 
 // let time = 0;
 
